194252d19ddb6a76ea228b022d3220e1fc2cbbaa
Yeswanth <yeswanthkumar.088@gmail.com>
2026-03-26 00:47:29 +0530

    Updated

--- ShopForHome_backend/ShopForHome/obj/Debug/net10.0/ShopForHome.AssemblyInfo.cs
+++ ShopForHome_backend/ShopForHome/obj/Debug/net10.0/ShopForHome.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ShopForHome")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c79035db311bbe9bae2cd8cec4850f1f8d2352a3")]
 [assembly: System.Reflection.AssemblyProductAttribute("ShopForHome")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ShopForHome")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
